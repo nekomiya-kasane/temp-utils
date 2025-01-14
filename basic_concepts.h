d0708adc0e8d2d88e5b0be9aa566564c0ada9245
猫宮かさね <nekomiya.kasane@gmail.com>
2025-01-14 20:02:27 +0800

    some concepts

--- basic_concepts.h
+++ basic_concepts.h
@@ -10,4 +10,11 @@ concept is_formattable = requires(std::format_context ctx, T value) {
   std::formatter<std::remove_cvref_t<T>>{}.format(value, ctx);
 };
 
+template<typename T>
+concept is_ranged = requires {
+  std::begin(std::declval<T>());
+  std::end(std::declval<T>());
+  std::ranges::subrange(std::begin(std::declval<T>()), std::end(std::declval<T>()));
+};
+
 }  // namespace traits
